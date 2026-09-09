36ca335e58e29ee1418a64741551514dc2211528
Atinuke Oyabambi <oyabambiatinuke-hash>
2026-09-09 00:54:18 -0700

    Fix HEXA runtime auth error

--- src/App.jsx
+++ src/App.jsx
@@ -1306,16 +1306,6 @@ function Sidebar({
                 @{profile?.username || "hexauser"}
               </span>
             </div>
-const savedPassword = localStorage.getItem(
-  `hexa-password-${user.id}`
-);
-
-if (!savedPassword) 
-  setShowHexaPasswordSetup(true);
- else 
-  setShowHexaPasswordLock(true)
-
-
           </div>
         </div>
       </aside>
