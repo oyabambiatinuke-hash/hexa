a02df8b349732abaabc8fb392a67ae822f451b57
Atinuke Oyabambi <oyabambiatinuke-hash>
2026-09-11 00:46:12 -0700

    Fix HEXA undefined user runtime crash

--- src/App.jsx
+++ src/App.jsx
@@ -1314,16 +1314,6 @@ function Sidebar({
                 @{profile?.username || "hexauser"}
               </span>
             </div>
-const savedPassword = localStorage.getItem(
-  `hexa-password-${user.id}`
-);
-
-if (!savedPassword) 
-  setShowHexaPasswordSetup(true);
- else 
-  setShowHexaPasswordLock(true)
-
-
           </div>
         </div>
       </aside>
