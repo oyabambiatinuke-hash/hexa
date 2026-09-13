334fdd9018b53a3f8f878311ccec977b344cce97
Atinuke Oyabambi <oyabambiatinuke-hash>
2026-09-13 01:07:50 -0700

    Fix HEXACHI Kora and connect OpenAI now like

--- src/App.jsx
+++ src/App.jsx
@@ -1337,14 +1337,7 @@ function Sidebar({
                 @{profile?.username || "hexauser"}
               </span>
             </div>
-const savedPassword = localStorage.getItem(
-  `hexa-password-${user.id}`
-);
 
-if (!savedPassword) 
-  setShowHexaPasswordSetup(true);
- else 
-  setShowHexaPasswordLock(true)
 
 
           </div>
